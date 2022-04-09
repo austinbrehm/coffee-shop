23d4c0c13730077a140330d7c047c6502bfd61cf
Austin Brehm <austin.brehm@gmail.com>
2022-04-09 13:19:17 -0500

    add menu

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-class Coffee:
-    def __init__(self, size):
-        self.size = size
+def main():
+    class Coffee:
+        def __init__(self, size):
+            self.size = size
 
-    def print_size(self):
-        print(f"Size: {self.size}")
+        def print_size(self):
+            print(f"Size: {self.size}")
+
+    class Latte(Coffee):
+        def __init__(self, size, espresso, milk, flavor):
+            super().__init__(size)
+            self.espresso = espresso
+            self.milk = milk
+            self.flavor = flavor
+
+    class Cappuccino(Coffee):
+        def __init__(self, size, espresso, milk, flavor):
+            super().__init__(size)
+            self.espresso = espresso
+            self.milk = milk
+            self.flavor = flavor
+
+    class DripBrew(Coffee):
+        def __init__(self, size, roast, creamer, sugar):
+            super().__init__(size)
+            self.roast = roast
+            self.creamer = creamer
+            self.sugar = sugar
+
+    def menu():
+        print("\nMain Menu\n"
+              "1. Order\n"
+              "2. Exit"
+              )
+    print('\nWelcome to the coffee shop!')
+    new_order = Latte('small', 'regular', '2%', 'none')
+
+    while True:
+        menu()
+        option = int(input('Select an option: '))
+        if option == 1:
+            pass
+        if option == 2:
+            print('Thank you for visiting the coffee shop. Good bye!')
+            break
 
 
-class Latte(Coffee):
-    def __init__(self, size, espresso, milk, flavor):
-        super().__init__(size)
-        self.espresso = espresso
-        self.milk = milk
-        self.flavor = flavor
-
-
-class Cappuccino(Coffee):
-    def __init__(self, size, espresso, milk, flavor):
-        super().__init__(size)
-        self.espresso = espresso
-        self.milk = milk
-        self.flavor = flavor
-
-
-class DripBrew(Coffee):
-    def __init__(self, size, roast, creamer, sugar):
-        super().__init__(size)
-        self.roast = roast
-        self.creamer = creamer
-        self.sugar = sugar
-
-
-new_order = Latte('small', 'regula', '2%', 'none')
-new_order.print_size()
+if __name__ == '__main__':
+    main()
